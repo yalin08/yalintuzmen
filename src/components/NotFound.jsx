2c31df5e80b2fff4c3cf6e082ae36abc624cc9be
Yalın <yalintuzmen08@gmail.com>
2024-09-03 23:45:05 +0300

    notfound componenti localize edildi

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../style/NotFound.scss';
 import sadImage from '../assets/img/sad.gif';
+import { useTranslation } from 'react-i18next';
 const NotFound = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="not-found">
             <div className="not-found-container">
                 <div className='textbtn'>
                     <h1 className="not-found-title">404</h1>
-                    <p className="not-found-message">Page Not Found</p>
-                    <a href="/" className="not-found-link">Go Back Home</a>
+                    <p className="not-found-message">{t('NotFound.NotFound')}</p>
+                    <a href="/" className="not-found-link">{t('NotFound.GoBack')}</a>
                 </div>
 
                 <div className='image'>
